d0f0ebf6f0772a8fb382c10956c674e971b3a48d
Miles Bailey-Braendgaard <milesjbb@gmail.com>
2022-09-15 14:24:30 +0100

    added company links and small styling

--- src/components/Favourites.jsx
+++ src/components/Favourites.jsx
@@ -1,23 +1,34 @@
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { removeFavourite } from '../features/favourite/favouriteSlice';
 
 const Favourites = () => {
   const favourites = useSelector((state) => state.favourite.favourites);
   console.log(favourites);
+  const dispatch = useDispatch();
 
   useEffect(() => {}, []);
 
   return (
-    <>
+    <div className="w-75 text-center mx-auto"> 
       <h1>FAVOURITES</h1>
       <ul>
         {favourites.map((company, i) => {
-          return (<li key={i}>{company}</li>)
+          return (
+            <li
+              key={i}
+              className="mx-0 mt-3 p-3"
+              style={{ border: "1px solid #00000033", borderRadius: 4 }}
+            >
+              <Link to="/${company}">{company}</Link><Button className="ml-5" variant="danger" onClick={() => {dispatch(removeFavourite(company))}}>Remove</Button>
+            </li>
+          );
         })}
       </ul>
       <Link to="/">Back to Search</Link>
-    </>
+    </div>
   );
 };
 
