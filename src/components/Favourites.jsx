e1d9ab48d52d9126abbb404fbc9a4082d0826e06
Miles Bailey-Braendgaard <milesjbb@gmail.com>
2022-09-15 14:52:28 +0100

    final styling

--- src/components/Favourites.jsx
+++ src/components/Favourites.jsx
@@ -1,8 +1,9 @@
 import { useEffect } from "react";
-import { Button } from "react-bootstrap";
+import { Button, Image } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { removeFavourite } from '../features/favourite/favouriteSlice';
+import { removeFavourite } from "../features/favourite/favouriteSlice";
+import dog from "../assets/saddog3.png"
 
 const Favourites = () => {
   const favourites = useSelector((state) => state.favourite.favourites);
@@ -12,25 +13,38 @@ const Favourites = () => {
   useEffect(() => {}, []);
 
   return (
-    <div className="w-75 text-center mx-auto my-3"> 
+    <div className="w-75 text-center mx-auto my-3">
       <h1 className="mt-3">Favourites</h1>
-      {(favourites.length === 0)? <h4 className="my-5">No favourites yet, go and find some!</h4> : 
-      <ul>
-        {favourites.map((company, i) => {
-          return (
-            <li
-              key={i}
-              className="mx-0 mt-3 p-3"
-              style={{ border: "1px solid #00000033", borderRadius: 4 }}
-            >
-              <Link to="/${company}">{company}</Link><Button className="ml-5" variant="danger" onClick={() => {dispatch(removeFavourite(company))}}>Remove</Button>
-            </li>
-          );
-        })}
-      </ul>
+      {favourites.length === 0 ? (
+        <div className="my-5">
+          <h4 >No favourites yet, go and find some!</h4>
+          <Image src={dog} style={{width: "10rem"}} rounded />
+        </div>
+      ) : (
+        <ul style={{listStyleType: "none"}}>
+          {favourites.map((company, i) => {
+            return (
+              <li
+                key={i}
+                className="mx-0 mt-3 p-3"
+                style={{ border: "1px solid #00000033", borderRadius: 4 }}
+              >
+                <Link to="/${company}">{company}</Link>
+                <Button
+                  className="ml-5"
+                  variant="danger"
+                  onClick={() => {
+                    dispatch(removeFavourite(company));
+                  }}
+                >
+                  Remove
+                </Button>
+              </li>
+            );
+          })}
+        </ul>
+      )}
 
-      }
-      
       <Link to="/">Back to Search</Link>
     </div>
   );
