197968583dc487e6431295eefdb475c9c8cd42df
Miles Bailey-Braendgaard <milesjbb@gmail.com>
2022-09-19 11:29:35 +0100

    small changes

--- src/features/favourite/favouriteSlice.js
+++ src/features/favourite/favouriteSlice.js
@@ -7,10 +7,6 @@ export const favouriteSlice = createSlice({
   },
   reducers: {
     addFavourite: (state, action) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
       state.favourites.push(action.payload)
     },
     removeFavourite: (state, action) => {
@@ -19,7 +15,6 @@ export const favouriteSlice = createSlice({
   },
 })
 
-// Action creators are generated for each case reducer function
 export const { addFavourite, removeFavourite } = favouriteSlice.actions
 
 export default favouriteSlice.reducer
